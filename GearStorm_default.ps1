2f7b7af71d367323ca16588885f324f2b241d4c5
Robomikel <michaelawilliams28@outlook.com>
2020-10-26 23:49:58 -0600

    Updated - added gearstorm risingstorm2

---
+++ GearStorm_default.ps1
@@ -0,0 +1,57 @@
+Function New-LaunchScriptGearStormPS {
+    # GearStorm Dedicated Server
+    # APP ID # 696120
+    ################## Change Default Variables #################
+    #                       Server IP
+    # ${global:IP}            = "${ip}"
+    #                       Server Port
+    $global:port            = "7777"
+    #                       Query Port
+    $global:queryport       = "27015"
+    #                       Map
+    $global:defaultmap      = "Austrasia"
+    #                       Maxplayers
+    $global:maxplayers      = "20"
+    #                       Server Name
+    $global:hostname        = "SERVERNAME"
+    ##############################/\##############################
+    ###################### Do not change below #####################
+    #                       System Directory
+    $global:systemdir       = "$serverdir"
+    #                       Server Config Directory
+    $global:servercfgdir    = "$serverdir\GearStorm\Saved\Config\WindowsServer"
+    #                       Server Executable
+    $global:executable      = "GearStormServer"
+    #                       Server Executable Directory
+    $global:executabledir   = "$serverdir\GearStorm\Binaries\Win64"
+    #                       Gamedig Query
+    $global:querytype       = "protocol-valve"
+    #                       Game Process
+    $global:process         = "GearStormServer"
+    #                       Log Directory
+    $global:logdirectory    = "$serverdir\GearStorm\Saved\Logs"
+    #                       Server Log
+    $global:consolelog      = "GearStorm.log"
+    #                       Game-Server-Config Directory
+    $global:gamedirname     = ""
+    #                       Game-Server-Config
+    $global:servercfg       = "Game.ini"
+    #                       Server Launch Command
+    $global:launchParams    = '@("${executable} /game/maps/${defaultmap}?Port=${port}?QueryPort=${queryport}?MaxPlayers=${maxplayers} -log")'
+    #                                           /game/maps/Austrasia?Port=7777?QueryPort=27015?MaxPlayers=20 -log
+  
+    # Get User Input version must be set to 0
+    Get-UserInput
+    # Download Game-Server-Config
+    # Get-Servercfg
+    # Edit Server Game-Server-Config
+    # Select-EditSourceCFG
+    $gameini = "$servercfgdir\Example_Configs\Example_$servercfg"
+    $engineini = "$servercfgdir\Example_Configs\Example_Engine.ini"
+    Write-log "`$gameini = $servercfgdir\Example_Configs\Example_$servercfg"
+    Write-log "`$engineini = $servercfgdir\Example_Configs\Example_Engine.ini"
+    If ((Test-path $gameini) -and (Test-Path $engineini)) {
+        Copy-Item $gameini  $servercfgdir\$servercfg -Force
+        Copy-Item $engineini $servercfgdir\Engine.ini -Force
+    }
+} 
---
+++ RisingStorm2_default.ps1
@@ -0,0 +1,51 @@
+Function New-LaunchScriptRS2PS {
+    # Rising Storm 2 - Dedicated Server
+    # APP ID # 418480
+    # https://wiki.rs2vietnam.com/index.php?title=DedicatedServer
+    ################## Change Default Variables #################
+    #                       Server IP
+    ${global:IP}            = "${ip}"
+    #                       Server Port
+    $global:port            = "7777"
+    #                       Query Port
+    $global:queryport       = "27015"
+    #                       Map
+    $global:defaultmap      = "VNSU-SongBe"
+    #                       Maxplayers
+    $global:maxplayers      = "64"
+    #                       Server Name
+    $global:hostname        = "SERVERNAME"
+    #                       Admin Password
+    $global:adminpassword   = "$RANDOMPASSWORD"
+    ##############################/\##############################
+    ###################### Do not change below #####################
+    #                       System Directory
+    $global:systemdir       = "$serverdir"
+    #                       Server Config Directory
+    $global:servercfgdir    = "$serverdir\ROGame\Config"
+    #                       Server Executable
+    $global:executable      = "VNGame"
+    #                       Server Executable Directory
+    $global:executabledir   = "$serverdir\Binaries\Win64"
+    #                       Gamedig Query
+    $global:querytype       = "protocol-valve"
+    #                       Game Process
+    $global:process         = "VNGame"
+    #                       Log Directory
+    $global:logdirectory    = "$serverdir\ROGame\Logs"
+    #                       Server Log
+    $global:consolelog      = "ServerLog.log"
+    #                       Game-Server-Config Directory
+    $global:gamedirname     = ""
+    #                       Game-Server-Config
+    $global:servercfg       = "ROGame.ini"
+    #                       Server Launch Command
+    $global:launchParams    = '@("${executable} ${defaultmap}?MaxPlayers=${maxplayers}Port=${port}?QueryPort=${queryport}?Multihome=${ip}?EnableWebAdmin=true?WebAdminPort=8080?AdminPassword=${adminpassword} -log=${consolelog} -UNATTENDED")'
+    #                       .\Binaries\Win64\VNGame.exe VNSU-SongBe?MaxPlayers=64?EnableWebAdmin=true?WebAdminPort=8080?AdminPassword=yourpasshere -log=ServerLog.log -UNATTENDED
+    # Get User Input version must be set to 0
+    Get-UserInput
+    # Download Game-Server-Config
+    # Get-Servercfg
+    # Edit Server Game-Server-Config
+    # Select-EditSourceCFG
+} 
