8de8e7fa05f781e9267256fcf70602eb6b971133
Robomikel <michaelawilliams28@outlook.com>
2020-11-01 08:02:11 -0700

    updated - added Astroneer, Badlads

---
+++ Astro_default.ps1
@@ -0,0 +1,77 @@
+Function New-LaunchScriptAstroPS {
+    # ASTRONEER Dedicated Server
+    # APP ID # 728470
+    ################## Change Default Variables #################
+    #                       Ext Server IP
+    ${global:IP}            = "${ip}"
+    #                       Server Port
+    $global:port            = "7777"
+    #                       Query Port
+    $global:queryport       = "27015"
+    #                       Server Name
+    $global:hostname        = "SERVERNAME"
+    #                       Server Password
+    $global:serverpassword  = " "
+    ##############################/\##############################
+    #Engine.ini 
+    #[URL]
+    #Port=7777
+    #   7777-7778
+
+    #AstroServerSettings.ini
+    #PublicIP=10.10.10.100
+    #OwnerName=yoursteamname
+    #OwnerGuid=0
+    ###################### Do not change below #####################
+    #                       System Directory
+    $global:systemdir       = "$serverdir"
+    #                       Server Config Directory
+    $global:servercfgdir    = "$serverdir\Astro\Saved\Config\WindowsServer"
+    #                       Server Executable
+    $global:executable      = "AstroServer-Win64-Shipping"
+    #                       Server Executable Directory
+    $global:executabledir   = "$serverdir\Astro\Binaries\Win64"
+    #                       Gamedig Query
+    $global:querytype       = "protocol-valve"
+    #                       Game Process
+    $global:process         = "AstroServer-Win64-Shipping"
+    #                       Log Directory
+    $global:logdirectory    = "$serverdir\Astro\Saved\Logs"
+    #                       Server Log
+    $global:consolelog      = "Astro.log"
+    #                       Game-Server-Config Directory
+    $global:gamedirname     = ""
+    #                       Game-Server-Config
+    $global:servercfg       = "AstroServerSettings.ini"
+    #                       Game-Server-Config
+    $global:config2       = "Engine.ini"
+    #                       Server Launch Command
+    $global:launchParams    = '@("${executable} -log")'
+    #                             AstroServer.exe
+  
+    # Get User Input version must be set to 0
+    Get-UserInput
+    # Download Game-Server-Config
+    # Get-Servercfg
+    # Edit Server Game-Server-Config
+    # Select-EditSourceCFG
+    # Install Adjustment
+    Set-Location $executabledir
+    Start-Process cmd "/c start AstroServer-Win64-Shipping.exe"
+    Start-sleep -Seconds 30
+    Get-StopServer    
+    Get-asserverInstallChanges
+    Set-Location $currentdir
+}
+
+Function Get-asserverInstallChanges {
+    if ($servercfg) {
+        $serverconfig = (gc $servercfgdir\$servercfg).Replace("ServerName=","ServerName=$hostname").Replace("PublicIP=","PublicIP=$extip").Replace("ServerPassword=$password","ServerPassword=") | Set-Content $servercfgdir\$servercfg -Force
+    }
+    if (Test-Path $servercfgdir\$config2) {
+       # New-Item $servercfgdir\$config2 -Force
+        Add-Content   $servercfgdir\$config2 `
+        "[URL]
+Port=$port"
+    }
+}
---
+++ BadLads_default.ps1
@@ -0,0 +1,43 @@
+Function New-LaunchScriptBadLadsPS {
+    # BadLads Dedicated Server
+    # APP ID # 1203110
+    ################## Change Default Variables #################
+    #                       Server IP
+    # ${global:IP}            = "${ip}"
+    #                       Server Port
+    $global:port            = "7777"
+    #                       Server Name
+    $global:hostname        = "SERVERNAME"
+    ##############################/\##############################
+    ###################### Do not change below #####################
+    #                       System Directory
+    $global:systemdir       = "$serverdir"
+    #                       Server Config Directory
+    $global:servercfgdir    = "$serverdir\BadLads\Saved\Config\WindowsServer"
+    #                       Server Executable
+    $global:executable      = "BadLadsServer-Win64-Shipping"
+    #                       Server Executable Directory
+    $global:executabledir   = "$serverdir\BadLads\Binaries\Win64"
+    #                       Gamedig Query
+    $global:querytype       = "protocol-valve"
+    #                       Game Process
+    $global:process         = "BadLadsServer-Win64-Shipping"
+    #                       Log Directory
+    $global:logdirectory    = "$serverdir\BadLads\Saved\Logs"
+    #                       Server Log
+    $global:consolelog      = "BadLads.log"
+    #                       Game-Server-Config Directory
+    $global:gamedirname     = ""
+    #                       Game-Server-Config
+    $global:servercfg       = "BadLadsServerSettings.ini"
+    #                       Server Launch Command
+    $global:launchParams    = '@("${executable} -port=${port}? -useperfthreads -SteamServerName=${hostname} -log")'
+    #                             BadLadsServer.exe -log -port=7777 -useperfthreads -SteamServerName=My-Server-Name
+  
+    # Get User Input version must be set to 0
+    Get-UserInput
+    # Download Game-Server-Config
+    # Get-Servercfg
+    # Edit Server Game-Server-Config
+    # Select-EditSourceCFG
+} 
