06d4a38afe8b4cc7d68e0690c665b6e0414c73ee
Robomikel <michaelawilliams28@outlook.com>
2020-12-29 10:07:14 -0700

    updated - missing server.cfg

--- TheForest_default.ps1
+++ TheForest_default.ps1
@@ -36,7 +36,9 @@ Function New-LaunchScriptforestserverPS {
     #                       Log Directory
     $global:logdirectory    = "$serverdir"
     #                       Server Log
-    $global:consolelog      = "console.log"  
+    $global:consolelog      = "console.log"
+    #                       Game-Server-Config
+    $global:servercfg       = "server.cfg"  
     #                       Server Launch Command
     $global:launchParams    = '@("${executable} -serverip ${ip} -serversteamport ${steamport} -servergameport ${port} -serverqueryport ${queryport} -servername `"${hostname}`" -serverplayers ${maxplayers} -difficulty ${diff} -configfilepath ${servercfgdir}\${servercfg} -inittype Continue -slot 4 -batchmode -nographics")'
     # Get User Input version must be set to 0
