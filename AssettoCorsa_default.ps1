a6148a6e7a7ac35d37aee284a02feb09b8333b64
Robomikel <michaelawilliams28@outlook.com>
2023-05-01 23:47:11 -0600

    updated - Assetto Corsa

--- AssettoCorsa_default.ps1
+++ AssettoCorsa_default.ps1
@@ -8,26 +8,29 @@ Function New-LaunchScriptacserverPS {
     
     ##############################/\##############################
     
-    
-    
+    #TCP/UDP 9600
+    #TCP 8081
+    #Change executable and process to AssettoServer, if mod installed
     
     ###################### Do not change below #####################
     #                           System Directory
     $global:systemdir           = "$serverdir"
-    #                           Server Executable
-    $global:executable          = "acServer.bat"
+    #                           Server Config Directory
+    $global:servercfgdir        = "$serverdir\cfg"
+    #                           Server Executable. Use "AssettoServer" if Mod installed
+    $global:executable          = "acServer"
     #                           Server Executable Directory
     $global:executabledir       = "$serverdir"
     #                           Gamedig Query
-    $global:querytype           = "protocol-valve"
-    #                           Game Process
+    $global:querytype           = "assettocorsa"
+    #                           Game Process. Use "AssettoServer" if Mod installed
     $global:process             = "acServer"
     #                           Log Directory
-    $global:logdirectory        = "$serverdir"
+    $global:logdirectory        = "$serverdir\logs"
     #                           Server Log
-    $global:consolelog          = ""
-    #                           Server Config Directory
-    $global:servercfgdir        = ""
+    $global:consolelog          = "output_*.log"
+    #                           Game-Server-Config
+    $global:servercfg           = "server_cfg.ini"
     #                           Server Launch Command
     $global:launchParams        = '@("${executable}")'
     # Advanced must be set to "0"
