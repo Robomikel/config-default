81c405e1238d720ce06b56ba5496cf8d7e444d02
Robomikel <michaelawilliams28@outlook.com>
2024-10-11 14:23:52 -0600

    updated

--- Valheim_default.ps1
+++ Valheim_default.ps1
@@ -42,15 +42,15 @@ Function New-LaunchScriptvalheimPS {
     #                               Log Directory
     $global:logdirectory            = "$serverdir"
     #                               Server Log
-    $global:consolelog              = "Serverlog.log"
+    $global:consolelog              = "Serverlog-*.log"
     #                               Game-Server-Config Directory
     $global:gamedirname             = "$serverdir"
     #                               Game-Server-Config
     $global:servercfg               = "server.cfg"
     #                               Server Launch Command
-    #$global:launchParams            = '@("${executable} -nographics -batchmode -name ${hostname} -port ${port} -world ${defaultmap} -password ${serverpassword} -logfile ${logdirectory}\Serverlog-$date.log")'
+    #$global:launchParams           = '@("${executable} -nographics -batchmode -name ${hostname} -port ${port} -world ${defaultmap} -password ${serverpassword} -savedir [PATH] -logfile ${logdirectory}\Serverlog-$date.log")'
     # 
-    $global:launchParams            = '@("${executable} -nographics -batchmode -name `"${hostname}`" -port ${port} -world `"${defaultmap}`" -password `"${serverpassword}`" ")'
+    $global:launchParams            = @("${executable} -nographics -batchmode -name `"${hostname}`" -port ${port} -world `"${defaultmap}`" -password `"${serverpassword}`" -logfile ${logdirectory}\Serverlog-$date.log")
     #                                    valheim_server -nographics -batchmode -name "My server" -port 2456 -world "Dedicated" -password "secret"
     # Advanced must be set to "0"
     Get-UserInput
